fd98c94d2f1a300d915d65992ece5a67d1ae98dc
geart891 <daniel.dungngo@gmail.com>
2017-07-01 07:24:22 +0700

    refactor

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,52 +3,55 @@ import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
-const config = {
-  entry: resolve(__dirname, 'app/index.jsx'),
-  output: {
-    path: resolve(__dirname, 'public'),
-    filename: `[name]-bundle.js`,
-    publicPath: '',
-  },
-  devtool: 'eval',
-  devServer: {
-    historyApiFallback: true,
-  },
-  resolve: {
-    extensions: ['.json', '.js', '.jsx'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        include: resolve(__dirname, 'app'),
-        use: 'babel-loader',
-      },
-      {
-        test: /\.css$/,
-        include: resolve(__dirname, 'app'),
-        use: ['style-loader', 'css-loader'],
-      },
+export default env => {
+  const config = {
+    entry: resolve(__dirname, 'app/index.jsx'),
+    output: {
+      path: resolve(__dirname, 'public'),
+      filename: `[name]-bundle.js`,
+      publicPath: '',
+      pathinfo: true,
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+      historyApiFallback: true,
+    },
+    resolve: {
+      extensions: ['.json', '.js', '.jsx'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          include: resolve(__dirname, 'app'),
+          use: 'babel-loader',
+        },
+        {
+          test: /\.css$/,
+          include: resolve(__dirname, 'app'),
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: resolve(__dirname, 'app/index.html'),
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: resolve(__dirname, 'app/index.html'),
-    }),
-  ],
+  };
+  // Production config
+  if (env.prod) {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production'),
+        },
+      }),
+      new webpack.optimize.CommonsChunkPlugin({ name: 'manifest' }),
+      new BundleAnalyzerPlugin()
+    );
+    config.devtool = 'hidden-source-map';
+    config.output.pathinfo = false;
+  }
+  return config;
 };
-
-if (process.env.NODE_ENV === 'production') {
-  config.plugins.push(
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-      },
-    }),
-    new webpack.optimize.CommonsChunkPlugin({ name: 'manifest' }),
-    new BundleAnalyzerPlugin()
-  );
-  config.devtool = 'hidden-source-map';
-}
-
-export default config;
